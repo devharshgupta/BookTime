05cd3874b4b7a5fc68e52db2afcb9ffb85154695
devharshgupta <hiharsh1999@gmail.com>
2024-05-12 21:19:15 +0530

    added query to fetch unit schedule

--- src/unit/controllers/unit-schedule.controller.ts
+++ src/unit/controllers/unit-schedule.controller.ts
@@ -1,27 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UnitScheduleService } from '../services/unit-schedule.service';
 
-@Controller('unit-schedule')
+@Controller('v1/unit-schedule')
 export class UnitScheduleController {
   constructor(private readonly unitScheduleService: UnitScheduleService) {}
 
-  // @Get()
-  // async findAll(): Promise<UnitSchedule[]> {
-  //     return this.unitScheduleService.findAll();
-  // }
-
-  // @Get(':id')
-  // async findOne(@Param('id') id: string): Promise<UnitSchedule | undefined> {
-  //     return this.unitScheduleService.findOne(+id);
-  // }
-
-  // @Put(':id')
-  // async update(@Param('id') id: string, @Body() updateUnitScheduleDto: UpdateUnitScheduleDto): Promise<UnitSchedule | undefined> {
-  //     return this.unitScheduleService.update(id, updateUnitScheduleDto);
-  // }
-
-  // @Delete(':id')
-  // async remove(@Param('id') id: string): Promise<void> {
-  //     await this.unitScheduleService.remove(+id);
-  // }
+  // TODO : add query range
+  @Get(':externalUnitId')
+  async findOne(@Param('externalUnitId') externalUnitId: string) {
+    return this.unitScheduleService.getUnitSchedule(externalUnitId);
+  }
 }
--- src/unit/services/unit-schedule.service.ts
+++ src/unit/services/unit-schedule.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { UnitLeaves } from '../entities/unit-leaves.entity';
 import { UnitSchedule } from '../entities/unit-schedule.entity';
+import { Unit } from '../entities/unit.entity';
 
 @Injectable()
 export class UnitScheduleService {
@@ -10,29 +12,51 @@ export class UnitScheduleService {
     private unitScheduleRepository: Repository<UnitSchedule>,
   ) {}
 
-  // async create(createUnitScheduleDto: CreateUnitScheduleDto): Promise<UnitSchedule> {
-  //     const unitSchedule = this.unitScheduleRepository.create(createUnitScheduleDto);
-  //     return await this.unitScheduleRepository.save(unitSchedule);
-  // }
-
   async findAll(): Promise<UnitSchedule[]> {
     return await this.unitScheduleRepository.find();
   }
 
-  // async findOne(id: number): Promise<UnitSchedule | undefined> {
-  //     return await this.unitScheduleRepository.findOne(id);
-  // }
+  async getUnitSchedule(externalUnitId: string) {
+    const startDate = '2024-05-14';
+    const endDate = '2024-05-24';
+    // const externalUnitId = 5;
 
-  // async update(unitId: string, updateUnitScheduleDto: UpdateUnitScheduleDto): Promise<UnitSchedule | undefined> {
-  //     const unitSchedule = await this.unitScheduleRepository.findOne(unitId);
-  //     if (!unitSchedule) {
-  //         return undefined;
-  //     }
-  //     const updatedUnitSchedule = Object.assign(unitSchedule, updateUnitScheduleDto);
-  //     return await this.unitScheduleRepository.save(updatedUnitSchedule);
-  // }
+    const query = this.unitScheduleRepository
+      .createQueryBuilder('unit_schedule')
+      .select([
+        'unit_schedule.startTime as startTime ',
+        'unit_schedule.endTime as endTime',
+        'unit_schedule.maxBooking as maxBooking',
+        'unit_schedule.metaText as metaText',
+        'unit_schedule.slotDuration as slotDuration',
+        "DATE_FORMAT(calendarDate, '%Y-%m-%d') AS calendarDate",
+        'calendarDay',
+      ])
+      .innerJoin(
+        'calendar',
+        'calendar',
+        'calendar.calendarDay = unit_schedule.weekDayName',
+      )
+      .innerJoin(Unit, 'unit', 'unit_schedule.unitId = unit.id')
+      .leftJoin(UnitLeaves, 'unit_leaves', 'unit.id = unit_leaves.unitId')
+      .where('unit.externalUnitId = :externalUnitId', { externalUnitId })
+      .andWhere('calendar.calendarDate BETWEEN :startDate AND :endDate', {
+        startDate,
+        endDate,
+      })
+      .andWhere('calendar.calendarDate BETWEEN unit.startDate AND unit.endDate')
+      .andWhere(
+        '(unit_leaves.unitId IS NULL OR ' +
+          '((unit_leaves.startDate > calendar.calendarDate) OR ' +
+          '(unit_leaves.endDate < calendar.calendarDate) OR ' +
+          '(unit_leaves.startDate = calendar.calendarDate AND unit_schedule.startTime < unit_leaves.startTime) OR ' +
+          '(unit_leaves.endDate <= calendar.calendarDate AND unit_schedule.startTime > unit_leaves.endTime)))',
+      )
+      .orderBy('calendar.calendarDate')
+      .addOrderBy('unit_schedule.startTime');
 
-  // async remove(id: number): Promise<void> {
-  //     await this.unitScheduleRepository.delete(id);
-  // }
+    const results = await query.getRawMany();
+
+    return results;
+  }
 }
