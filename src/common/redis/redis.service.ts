ccddd2a716b9fc9c5c464ff84420bdf1456c90f3
devharshgupta <hiharsh1999@gmail.com>
2024-05-14 09:19:50 +0530

    added redis methods

--- src/common/redis/redis.service.ts
+++ src/common/redis/redis.service.ts
@@ -5,22 +5,84 @@ import { Redis } from 'ioredis';
 
 @Injectable()
 export class RedisService {
-  constructor(@Inject('REDIS_CLIENT') private readonly redisClient: Redis) {}
+  constructor(@Inject('REDIS_CLIENT') private readonly client: Redis) {}
 
   async set(
     key: string,
     value: string | object,
-    ttlInSecond = 10,
+    ttlInSecond: number,
   ): Promise<void> {
-    if (typeof value === 'object') value = JSON.stringify(value);
-    const result = await this.redisClient.set(key, value, 'EX', ttlInSecond);
+    Logger.log(key, value, ttlInSecond);
+    value = typeof value === 'string' ? value : JSON.stringify(value);
+    const result = await this.client.set(key, value, 'EX', ttlInSecond);
     Logger.log(`Redis Set key : ${key} `, result);
     if (result != 'OK') {
-      throw new Error();
+      throw new Error('Something went wrong with redis'); // or throw the error as per your needs
     }
   }
 
   async get(key: string): Promise<string | null> {
-    return this.redisClient.get(key);
+    Logger.log(`get key : ${key} `);
+    return this.client.get(key);
+  }
+
+  async getMultipleKeys(keys: string[]) {
+    try {
+      const values = await this.client.mget(keys);
+      return values; // Array containing values for the requested keys, or null if any key is missing
+    } catch (error) {
+      console.error('Error fetching multiple keys from Redis:', error);
+      // Handle errors appropriately, e.g., return an empty array or throw a custom error
+      return []; // Or throw an error depending on your needs
+    }
+  }
+
+  async getKeysByPattern(pattern: string): Promise<string[]> {
+    // const client = this.redisService.getClient();
+    const keys = await this.client.keys(pattern);
+    return keys;
+  }
+
+  async fetchValuesByPattern(pattern: string): Promise<object[]> {
+    let cursor = '0';
+    const keys: string[] = [];
+
+    // Use SCAN to iterate over keys matching the pattern
+    do {
+      const [newCursor, partialKeys] = await this.client.scan(
+        cursor,
+        'MATCH',
+        pattern,
+      );
+      keys.push(...partialKeys);
+      cursor = newCursor;
+    } while (cursor !== '0');
+
+    const data = await Promise.all(
+      keys.map(async (key) => JSON.parse(await this.client.get(key))),
+    );
+    return data;
+  }
+
+  // To set multiple key inside in transactional fasion
+  async setMultiObjectWithTtl(
+    data: { key: string; value: string | object; ttl: number }[],
+  ): Promise<void> {
+    try {
+      const commands = [];
+      for (const item of data) {
+        const redisKey = `${item.key}`; // Customize key generation if required
+        const valueToStore =
+          typeof item.value === 'string'
+            ? item.value
+            : JSON.stringify(item.value);
+        commands.push(this.client.set(redisKey, valueToStore));
+        commands.push(this.client.expire(redisKey, item.ttl));
+      }
+      await this.client.multi(...commands).exec();
+    } catch (err) {
+      console.error('Error setting objects with TTL:', err);
+      throw new Error(err); // Or handle the error differently based on your needs
+    }
   }
 }
